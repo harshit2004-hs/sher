6aa49624e93131919adb7f7d8d11365c1b986d8c
harshit2004-hs <harshitsalian6@gmail.com>
2024-10-21 12:17:31 +0530

    first commit~

---
+++ pyhton sum_fixed_numbers.py
@@ -0,0 +1,16 @@
+# sum_numbers.py
+ 
+import sys
+ 
+def main():
+    if len(sys.argv) != 3:
+        print("Please provide two numbers.")
+        return
+ 
+    first = int(sys.argv[1])
+    second = int(sys.argv[2])
+    sum_ = first + second
+    print(f"{first} + {second} = {sum_}")
+ 
+if __name__ == "__main__":
+    main()
